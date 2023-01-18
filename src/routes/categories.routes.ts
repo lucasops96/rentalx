e2bc4fcd6d1e65dbf7835b87c68631a00b4caed4
João Lucas <lucasbanana96@gmail.com>
2023-01-18 15:12:41 -0300

    docker comandos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,6 +12,7 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/",(request,response)=>{
+    console.log("Reload Funcionando");
     return createCategoryController.handle(request,response);
 });
 
